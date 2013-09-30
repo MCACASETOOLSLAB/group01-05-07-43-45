b7aba582e15b7f8d30462388f3aeb31917007b09
Group-20 <akanshasatyarthi212@gmail.com>
2013-09-30 02:08:53 -0700

    final_GUI

---
+++ Bathroom_Problem1/Bathroom_Problem1/Form1.Designer.vb
@@ -0,0 +1,309 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.p4 = New System.Windows.Forms.PictureBox()
+        Me.p1 = New System.Windows.Forms.PictureBox()
+        Me.p2 = New System.Windows.Forms.PictureBox()
+        Me.p3 = New System.Windows.Forms.PictureBox()
+        Me.p5 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.p6 = New System.Windows.Forms.PictureBox()
+        Me.p7 = New System.Windows.Forms.PictureBox()
+        Me.p8 = New System.Windows.Forms.PictureBox()
+        Me.p9 = New System.Windows.Forms.PictureBox()
+        Me.p10 = New System.Windows.Forms.PictureBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        CType(Me.p4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.p1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.p2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.p3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.p5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.p6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.p7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.p8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.p9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.p10, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'p4
+        '
+        Me.p4.Image = CType(resources.GetObject("p4.Image"), System.Drawing.Image)
+        Me.p4.Location = New System.Drawing.Point(353, 32)
+        Me.p4.Name = "p4"
+        Me.p4.Size = New System.Drawing.Size(139, 225)
+        Me.p4.TabIndex = 0
+        Me.p4.TabStop = False
+        '
+        'p1
+        '
+        Me.p1.Image = CType(resources.GetObject("p1.Image"), System.Drawing.Image)
+        Me.p1.Location = New System.Drawing.Point(60, 365)
+        Me.p1.Name = "p1"
+        Me.p1.Size = New System.Drawing.Size(108, 155)
+        Me.p1.TabIndex = 1
+        Me.p1.TabStop = False
+        '
+        'p2
+        '
+        Me.p2.Image = CType(resources.GetObject("p2.Image"), System.Drawing.Image)
+        Me.p2.Location = New System.Drawing.Point(215, 365)
+        Me.p2.Name = "p2"
+        Me.p2.Size = New System.Drawing.Size(107, 155)
+        Me.p2.TabIndex = 2
+        Me.p2.TabStop = False
+        '
+        'p3
+        '
+        Me.p3.Image = CType(resources.GetObject("p3.Image"), System.Drawing.Image)
+        Me.p3.Location = New System.Drawing.Point(353, 365)
+        Me.p3.Name = "p3"
+        Me.p3.Size = New System.Drawing.Size(114, 155)
+        Me.p3.TabIndex = 3
+        Me.p3.TabStop = False
+        '
+        'p5
+        '
+        Me.p5.Image = CType(resources.GetObject("p5.Image"), System.Drawing.Image)
+        Me.p5.Location = New System.Drawing.Point(81, 57)
+        Me.p5.Name = "p5"
+        Me.p5.Size = New System.Drawing.Size(220, 153)
+        Me.p5.TabIndex = 4
+        Me.p5.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(565, 71)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(82, 31)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(712, 71)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(82, 36)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Leave "
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'p6
+        '
+        Me.p6.Image = CType(resources.GetObject("p6.Image"), System.Drawing.Image)
+        Me.p6.Location = New System.Drawing.Point(60, 365)
+        Me.p6.Name = "p6"
+        Me.p6.Size = New System.Drawing.Size(108, 144)
+        Me.p6.TabIndex = 7
+        Me.p6.TabStop = False
+        Me.p6.Visible = False
+        '
+        'p7
+        '
+        Me.p7.Image = CType(resources.GetObject("p7.Image"), System.Drawing.Image)
+        Me.p7.Location = New System.Drawing.Point(215, 365)
+        Me.p7.Name = "p7"
+        Me.p7.Size = New System.Drawing.Size(107, 145)
+        Me.p7.TabIndex = 8
+        Me.p7.TabStop = False
+        Me.p7.Visible = False
+        '
+        'p8
+        '
+        Me.p8.Image = CType(resources.GetObject("p8.Image"), System.Drawing.Image)
+        Me.p8.Location = New System.Drawing.Point(353, 365)
+        Me.p8.Name = "p8"
+        Me.p8.Size = New System.Drawing.Size(114, 155)
+        Me.p8.TabIndex = 9
+        Me.p8.TabStop = False
+        Me.p8.Visible = False
+        '
+        'p9
+        '
+        Me.p9.Image = CType(resources.GetObject("p9.Image"), System.Drawing.Image)
+        Me.p9.Location = New System.Drawing.Point(353, 39)
+        Me.p9.Name = "p9"
+        Me.p9.Size = New System.Drawing.Size(104, 218)
+        Me.p9.TabIndex = 10
+        Me.p9.TabStop = False
+        '
+        'p10
+        '
+        Me.p10.Image = CType(resources.GetObject("p10.Image"), System.Drawing.Image)
+        Me.p10.Location = New System.Drawing.Point(81, 57)
+        Me.p10.Name = "p10"
+        Me.p10.Size = New System.Drawing.Size(220, 153)
+        Me.p10.TabIndex = 11
+        Me.p10.TabStop = False
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(565, 135)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(82, 33)
+        Me.Button3.TabIndex = 12
+        Me.Button3.Text = "Start"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(712, 135)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(82, 33)
+        Me.Button4.TabIndex = 13
+        Me.Button4.Text = "Leave"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(565, 205)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(82, 33)
+        Me.Button5.TabIndex = 14
+        Me.Button5.Text = "Start"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(712, 205)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(82, 33)
+        Me.Button6.TabIndex = 15
+        Me.Button6.Text = "Leave"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Lucida Handwriting", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(298, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(297, 21)
+        Me.Label1.TabIndex = 16
+        Me.Label1.Text = "SYNCHRONIZATION PROBLEM"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(110, 229)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 24)
+        Me.Label2.TabIndex = 17
+        Me.Label2.Text = "police"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(367, 279)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(49, 24)
+        Me.Label3.TabIndex = 18
+        Me.Label3.Text = "door"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(534, 421)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(204, 24)
+        Me.Label4.TabIndex = 19
+        Me.Label4.Text = "Men standing in queue"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.SeaShell
+        Me.ClientSize = New System.Drawing.Size(858, 521)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.p5)
+        Me.Controls.Add(Me.p3)
+        Me.Controls.Add(Me.p2)
+        Me.Controls.Add(Me.p1)
+        Me.Controls.Add(Me.p4)
+        Me.Controls.Add(Me.p6)
+        Me.Controls.Add(Me.p7)
+        Me.Controls.Add(Me.p8)
+        Me.Controls.Add(Me.p9)
+        Me.Controls.Add(Me.p10)
+        Me.Name = "Form1"
+        Me.Text = "Bathroom Problem"
+        CType(Me.p4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.p1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.p2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.p3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.p5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.p6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.p7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.p8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.p9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.p10, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents p4 As System.Windows.Forms.PictureBox
+    Friend WithEvents p1 As System.Windows.Forms.PictureBox
+    Friend WithEvents p2 As System.Windows.Forms.PictureBox
+    Friend WithEvents p3 As System.Windows.Forms.PictureBox
+    Friend WithEvents p5 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents p6 As System.Windows.Forms.PictureBox
+    Friend WithEvents p7 As System.Windows.Forms.PictureBox
+    Friend WithEvents p8 As System.Windows.Forms.PictureBox
+    Friend WithEvents p9 As System.Windows.Forms.PictureBox
+    Friend WithEvents p10 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+
+End Class
---
+++ Bathroom_Problem1/Bathroom_Problem1/Form1.vb
@@ -0,0 +1,144 @@
+﻿Public Class Form1
+    'Dim A1 As UInteger = 0.2, b1 As Integer
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub PictureBox1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles p4.Click
+
+    End Sub
+
+    Private Sub PictureBox5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles p5.Click
+
+    End Sub
+
+    Private Sub PictureBox2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles p1.Click
+
+    End Sub
+
+    Private Sub PictureBox3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles p2.Click
+
+    End Sub
+
+    Private Sub PictureBox4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles p3.Click
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Button3.Enabled = True
+        Button5.Enabled = True
+        Button1.Enabled = False
+        p1.Visible = False
+        p6.Visible = False
+        p4.Visible = True
+        p9.Visible = False
+        p5.Visible = True
+        p10.Visible = False
+        While (p2.Location.X >= 30)
+            p2.Left = p2.Left - 1
+            p3.Left = p3.Left - 1
+            p7.Left = p7.Left - 1
+            p8.Left = p8.Left - 1
+
+        End While
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        '  MsgBox("The current time is " & Now.ToShortTimeString)
+        Button3.Enabled = False
+        Button5.Enabled = False
+
+        While (p6.Location.X <= 330 And p6.Location.Y >= 55)
+
+            p6.Top = p6.Top - 1
+            p6.Left = p6.Left + 1
+            p1.Visible = False
+            p6.Visible = True
+            p4.Visible = False
+            p9.Visible = True
+            p5.Visible = False
+            p10.Visible = True
+        End While
+            End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Button1.Enabled = False
+        Button5.Enabled = False
+        Button2.Enabled = False
+        p6.Visible = False
+        p1.Visible = False
+        While (p7.Location.X <= 320 And p7.Location.Y >= 55)
+
+            p7.Top = p7.Top - 1
+            p7.Left = p7.Left + 1
+            p2.Visible = False
+            p7.Visible = True
+            p4.Visible = False
+            p9.Visible = True
+            p5.Visible = False
+            p10.Visible = True
+        End While
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        Button3.Enabled = False
+        Button5.Enabled = True
+
+        p1.Visible = False
+        p6.Visible = False
+        p4.Visible = True
+        p9.Visible = False
+        p5.Visible = True
+        p10.Visible = False
+
+        p7.Visible = False
+        While (p3.Location.X >= 30)
+            p3.Left = p3.Left - 1
+            p8.Left = p8.Left - 1
+
+        End While
+
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        Button3.Enabled = False
+        ' Button5.Enabled = False
+        Button4.Enabled = False
+        Button2.Enabled = False
+        p6.Visible = False
+        p1.Visible = False
+        p2.Visible = False
+        p7.Visible = False
+        While (p8.Location.X <= 320 And p8.Location.Y >= 25)
+
+            p8.Top = p8.Top - 1
+            p8.Left = p8.Left + 1
+            p8.Visible = True
+            p4.Visible = False
+            p3.Visible = False
+
+            p9.Visible = True
+            p5.Visible = False
+            p10.Visible = True
+        End While
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        Button5.Enabled = False
+        p3.Visible = False
+        p8.Visible = False
+        p4.Visible = True
+        p5.Visible = True
+        Button6.Enabled = False
+        'MsgBox("no more people in queue")
+
+    End Sub
+
+
+ 
+    Private Sub p9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles p9.Click
+
+    End Sub
+End Class
